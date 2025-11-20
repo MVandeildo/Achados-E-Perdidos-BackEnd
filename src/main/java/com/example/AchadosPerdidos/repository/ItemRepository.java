5cd661c94128c9dcebf2b2c54128cb6147fab0f5
MVandeildo <saopaulo.vandeildo883@gmail.com>
2025-11-20 18:01:01 -0300

    Subindo os repositorios do projeto

---
+++ src/main/java/com/example/AchadosPerdidos/repository/ItemRepository.java
@@ -0,0 +1,13 @@
+package com.example.AchadosPerdidos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.AchadosPerdidos.model.Item;
+import java.util.List;
+
+
+public interface ItemRepository extends JpaRepository<Item,Long> {
+
+    List<Item> findByStatus(String status);
+    List<Item> findByLocalizacaoContainingIgnoreCase(String localizacao);
+    
+}
